84fc647e5c54559e74a90859a64c54f22afd6c6e
markd <markd@ekconsulting.net>
2025-12-03 00:46:03 +0800

    traits attribute

--- src/Models/User.php
+++ src/Models/User.php
@@ -8,4 +8,39 @@ use Mdayo\User\Models\Traits\UserModelTrait;
 class User extends Authenticatable
 {
     use UserModelTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $guarded = [];
+ 
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'email_verified_at',
+        'created_at',
+        'updated_at'
+    ];
+    
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed'
+        ];
+    } 
+    protected $guard_name = 'user';
 }

38d4424aa175b7324fbfa03fe1df026e2a9121f5
markd <markd@ekconsulting.net>
2025-12-03 00:18:22 +0800

    make the package simple

--- src/UserServiceProvider.php
+++ src/UserServiceProvider.php
@@ -17,16 +17,7 @@ class UserServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // Load package routes
-        $this->loadRoutesFrom(__DIR__.'/../routes/user.php');
-        
-        $this->publishes([
-            __DIR__ . '/../routes/user.php' => base_path('routes/user.php')
-        ], 'user-routes');
-        
-        // No need user migration just use the default migration in laaravel ^11x
-        //$this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
+   
         // Publish config
         $this->publishes([
             __DIR__.'/../config/user.php' => config_path('user.php'),
